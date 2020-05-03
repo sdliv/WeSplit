3b7b2a7b31941c99c0855b109117109667a9fc74
Sean <sean@covertalert.ai>
2020-05-02 22:30:29 -0400

    Check spliter completed. Value concepts learned.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,11 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    // prperties with state observed
     @State private var checkoutAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
+
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkoutAmount) ?? 0
+            
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
@@ -21,16 +35,27 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkoutAmount)
                         .keyboardType(.decimalPad)
-                    
+                    // Picker with style determined by Apple, no 'pickerStyle'
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 100) {
                             Text("\($0) people")
                         }
                     }
                 }
+                // Section with a header
+                Section(header: Text("How much tip do you want to leave?")) {
+                    // Picker with segmented control picker style
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
                 
                 Section {
-                    Text("$\(checkoutAmount)")
+                    // total per person with two decimal place limit
+                    Text("$\(totalPerPerson, specifier: "%.2F")")
                 }
             }
             .navigationBarTitle("WeSplit")
